e45d402bb2b8a165081b5e3805973d9e5c0e4b80
Robert Maissan <thorkia@gmail.com>
2023-12-04 09:55:33 -0500

    Day 4 Solution

--- Day04/Day04.py
+++ Day04/Day04.py
@@ -2,24 +2,55 @@
 
 def Part1(filename: str):
     with open(filename) as f:
-        lines = [x for x in f.readlines()]
+        lines = [x.strip() for x in f.readlines()]
         f.close()
+    
+    totalPoints = 0
+    for line in lines:
+        line = line.split(':')
+        game = line[1].split('|')
 
-    return False
+        winningNums = set([ int(x) for x in game[0].strip().split(' ') if x!=''])
+        nums = set([ int(x) for x in game[1].strip().split(' ') if x!=''])
+
+        matches = winningNums.intersection(nums)
+        if len(matches) > 0:
+            totalPoints += 2**(len(matches)-1)
+    
+    print(totalPoints)    
 
 
 def Part2(filename: str):
     with open(filename) as f:
-        lines = [x for x in f.readlines()]
+        lines = [x.strip()for x in f.readlines()]
         f.close()
 
+    cardCounts = { }
+    for x in range(len(lines)):
+        cardCounts[x+1] = 1
+
+    cardNum = 0
+    for line in lines:
+        cardNum+=1
+        line = line.split(':')
+        game = line[1].split('|')
+
+        winningNums = set([ int(x) for x in game[0].strip().split(' ') if x!=''])
+        nums = set([ int(x) for x in game[1].strip().split(' ') if x!=''])
+
+        matches = winningNums.intersection(nums)
+        for x in range(len(matches)):
+            winCard = cardNum+x+1
+            cardCounts[winCard] += cardCounts[cardNum]
+
+    print(sum(cardCounts.values()))
     return False
 
 
 if __name__ == '__main__':    
     
-    filename = './Day04/test_input.txt'    
-    #filename = './Day04/input.txt'    
+    #filename = './Day04/test_input.txt'    
+    filename = './Day04/input.txt'    
     
     Part1(filename)
     Part2(filename)
